6d135cdbdb7fea3c6c0fdffa38fe912dd4735538
RachmanForniandi <rachmanforniandi@gmail.com>
2017-07-22 09:17:23 +0700

    buat user login di PHP

--- includes/DbOperations.php
+++ includes/DbOperations.php
@@ -28,12 +28,30 @@
 				}
 			}
 		}
+
+		public function userLogin($username, $pass){
+				$password = md5($pass);
+				$stmt = $this->connection->prepare("SELECT id FROM `users` WHERE username = ? AND password = ?");
+				$stmt->bind_param("ss",$username,$password);
+				$stmt->execute();
+				$stmt->store_result();
+				return $stmt->num_rows > 0;
+		}
+
+		public function getUserByUsername($username){
+			$stmt = $this->connection->prepare("SELECT * FROM users WHERE username = ?");
+			$stmt->bind_param("s", $username);
+			$stmt->execute();
+			return $stmt->get_result()->fetch_assoc();
+		}
+			
+
 		public function isUserExist($username, $email){
 			$stmt = $this->connection->prepare("SELECT id FROM users WHERE username= ? OR email=? ");
 			$stmt->bind_param("ss",$username,$email);
 			$stmt->execute();
-			stmt->store_result();
-			return $stmt->num_rows >0;
+			$stmt->store_result();
+			return $stmt->num_rows > 0;
 		}
 	}
 ?>
